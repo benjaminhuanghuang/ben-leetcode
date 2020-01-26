6ca7e9302469c7f279f9b263fd8749b3eaf61d3d
Benjamin Huang <bhuang@rms.com>
2020-01-25 19:36:46 -0800

    update

---
+++ code_skeleton/js/solution.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const hashTable = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const rest = target - nums[i];
+    if (rest in hashTable) {
+      return [i, hashTable[rest]];
+    }
+    hashTable[nums[i]] = i;
+  }
+  return [-1, -1]
+};
+
+
+export { twoSum };
