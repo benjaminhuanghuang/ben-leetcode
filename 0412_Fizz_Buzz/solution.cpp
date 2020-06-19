1b9ee99531ab5bd8d4b58bedc1ad599c0d35eed4
Benjamin Huang <bhuang@rms.com>
2020-06-18 21:10:29 -0700

    update

--- 0412_Fizz_Buzz/solution.cpp
+++ 0412_Fizz_Buzz/solution.cpp
@@ -16,14 +16,35 @@ https://leetcode.com/problems/fizz-buzz
 
 using namespace std;
 
-
 /*
   Solution: 
 */
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> fizzBuzz(int n) {
-        
+  vector<string> fizzBuzz(int n)
+  {
+    vector<string> ans;
+
+    for (int i = 1; i <= n; i++)
+    {
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        ans.push_back("FizzBuzz");
+      }
+      else if (i % 5 == 0)
+      {
+        ans.push_back("Buzz");
+      }
+      else if (i % 3 == 0)
+      {
+        ans.push_back("Fizz");
+      }
+      else{
+        ans.push_back(to_string(i));
+      }
     }
+    return ans;
+  }
 };
