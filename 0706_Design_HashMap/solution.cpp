adab63a5f5103bb4d7fb87bb43b6335913b1a1f8
Benjamin Huang <bhuang@rms.com>
2020-06-25 13:14:42 -0700

    update

--- 0706_Design_HashMap/solution.cpp
+++ 0706_Design_HashMap/solution.cpp
@@ -74,6 +74,59 @@ public:
   }
 };
 
+class MyHashMap2
+{
+public:
+  /** Initialize your data structure here. */
+  vector<pair<int, int>> m[1000];
+  MyHashMap2()
+  {
+  }
+
+  /** value will always be non-negative. */
+  void put(int key, int value)
+  {
+    int hashnum = key % 1000;
+    for (int i = 0; i < m[hashnum].size(); i++)
+    {
+      if (m[hashnum][i].first == key)
+      {
+        m[hashnum][i].second = value;
+        return;
+      }
+    }
+    m[hashnum].push_back({key, value});
+  }
+
+  /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+  int get(int key)
+  {
+    int hashnum = key % 1000;
+    for (int i = 0; i < m[hashnum].size(); i++)
+    {
+      if (m[hashnum][i].first == key)
+      {
+        return m[hashnum][i].second;
+      }
+    }
+    return -1;
+  }
+
+  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+  void remove(int key)
+  {
+    int hashnum = key % 1000;
+    for (int i = 0; i < m[hashnum].size(); i++)
+    {
+      if (m[hashnum][i].first == key)
+      {
+        m[hashnum].erase(m[hashnum].begin() + i);
+        return;
+      }
+    }
+  }
+};
+
 /**
  * Your MyHashMap object will be instantiated and called as such:
  * MyHashMap* obj = new MyHashMap();
--- 0720_Longest_Word_in_Dictionary/solution.cpp
+++ 0720_Longest_Word_in_Dictionary/solution.cpp
@@ -17,14 +17,52 @@ https://leetcode.com/problems/longest-word-in-dictionary
 
 using namespace std;
 
-
 /*
   Solution: 
 */
 
-class Solution {
+class Solution
+{
 public:
-    string longestWord(vector<string>& words) {
-        
+  string longestWord(vector<string> &words)
+  {
+    sort(words.begin(), words.end());
+
+    unordered_set<string> set;
+    string res = "";
+
+    for (string word : words)
+    {
+      if (word.length() == 1 || set.count(word.substr(0, word.length() - 1)))
+      {
+        // 遍历words，遇到长度等于 1的 word 或者 这个word 去掉最后一个char 已经存在于 set
+        res = word.length() > res.length() ? word : res; // keep the longer one
+        set.insert(word);
+      }
     }
+
+    return res;
+  }
+
+  string longestWord2(vector<string> &words)
+  {
+    string best;
+    unordered_set<string> dict(words.begin(), words.end());
+    for (const string &word : words)
+    {
+      if (word.length() < best.length() || (word.length() == best.length() && word > best))
+        continue;
+      string prefix;
+      bool valid = true;
+      for (int i = 0; i < word.length() - 1 && valid; ++i)
+      {
+        prefix += word[i];
+        if (!dict.count(prefix))
+          valid = false;
+      }
+      if (valid)
+        best = word;
+    }
+    return best;
+  }
 };
