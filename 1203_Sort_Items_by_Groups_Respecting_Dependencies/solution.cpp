72d64066ffb25b317613c9d37e9508f03722a2fb
Benjamin Huang <bhuang@rms.com>
2020-07-12 20:49:15 -0700

    update

--- 1203_Sort_Items_by_Groups_Respecting_Dependencies/solution.cpp
+++ 1203_Sort_Items_by_Groups_Respecting_Dependencies/solution.cpp
@@ -20,16 +20,114 @@ https://leetcode.com/problems/sort-items-by-groups-respecting-dependencies
 
 using namespace std;
 
-
 /*
   1. 属于同一group的item要排序
   2. before Items
   Solution: 
 */
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> sortItems(int n, int m, vector<int>& group, vector<vector<int>>& beforeItems) {
-        
+  vector<int> sortItems(int n, int m, vector<int> &group, vector<vector<int>> &beforeItems)
+  {
+    vector<vector<int>> gGraph(m, vector<int>());
+    vector<vector<int>> iGraph(n, vector<int>());
+
+    // item items do not belong to group to new group
+    for (int i = 0; i < group.size(); i++)
+    {
+      if (group[i] == -1)
+      {
+        // i-th item belongs to no group
+        group[i] = m++;
+      }
     }
+
+    vector<int> itemInDegree(n, 0);
+    vector<int> groupInDegree(m, 0); // m is increased abrove.
+
+    // build item graph
+    for (int to = 0; to < beforeItems.size(); to++)
+    {
+      int toGroup = group[to];
+
+      for (int from : beforeItems[to])
+      {
+        itemInDegree[to]++;
+        iGraph[from].push_back(to);
+      }
+    }
+    // build group graph
+    for (int to = 0; to < group.size(); to++)
+    {
+      int toGroup = group[to];
+
+      for (int from : beforeItems[to])
+      {
+        int fromGroup = group[to];
+        if (fromGroup != toGroup)
+        {
+          groupInDegree[toGroup]++;
+          gGraph[fromGroup].push_back(to);
+        }
+      }
+    }
+
+    vector<int> iList = tpSort(iGraph, itemInDegree, n);
+    vector<int> gList = tpSort(gGraph, groupInDegree, m);
+
+    if (iList.size() == 0 || gList.size() == 0)
+      return {};
+
+    vector<vector<int>> groupedList(m, vector<int>());
+
+    for (int item : iList)
+    {
+      int grp = group[item];
+
+      groupedList[grp].push_back(item);
+    }
+
+    vector<int> ans;
+    for (int grp : gList)
+    {
+      for (int item : groupedList[grp])
+      {
+        ans.push_back(item);
+      }
+    }
+    return ans;
+  }
+
+  vector<int> tpSort(vector<vector<int>> &graph, vector<int> &inDegree, int count)
+  {
+    vector<int> ans;
+    queue<int> q;
+
+    for (int i = 0; i < inDegree.size(); i++)
+    {
+      if (inDegree[i] == 0)
+      {
+        q.push(i);
+      }
+    }
+
+    while (!q.empty())
+    {
+      int cur = q.front();
+      q.pop();
+      ans.push_back(cur);
+
+      for (int next : graph[cur])
+      {
+        inDegree[next]--;
+        if (inDegree[next] == 0)
+        {
+          q.push(next);
+        }
+      }
+    }
+    return count == ans.size() ? ans : vector<int>();
+  }
 };
--- _playground/cpp/main.cpp
+++ _playground/cpp/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cmath>
 #include <map>
+#include <queue>
 #include <unordered_map>
 #include <unordered_set>
 #include <stack>
@@ -11,38 +12,113 @@
 #include <numeric>
 using namespace std;
 
-int main()
+vector<int> tpSort(vector<vector<int>> &graph, vector<int> &inDegree, int count)
 {
-  /* Enter your code here.  */
-  // int n;
-  // cin >> n;
-  // int nums[n];
+  vector<int> ans;
+  queue<int> q;
 
-  // for (int i = 0; i < n; i++)
-  // {
-  //   cin >> nums[i];
-  // }
-
-  int n = 5;
-  int nums[5] = {1, 5, 3, 8, 2};
-  int count = 0;
-  bool sorted = false;
-
-  while (!sorted)
+  for (int i = 0; i < count; i++)
   {
-    count++;
-    sorted = true;
-    for (int i = 0; i < n - 1; i++)
+    if (inDegree[i] == 0)
     {
-      if (nums[i] > nums[i + 1])
+      q.push(i);
+    }
+  }
+
+  while (!q.empty())
+  {
+    int cur = q.front();
+    q.pop();
+    ans.push_back(cur);
+
+    for (int next : graph[cur])
+    {
+      inDegree[next]--;
+      if (inDegree[next] == 0)
       {
-        swap(nums[i], nums[i + 1]);
-        sorted = false;
+        q.push(next);
       }
     }
   }
+  return count == ans.size() ? ans : vector<int>();
+}
+
+vector<int> sortItems(int n, int m, vector<int> &group, vector<vector<int>> &beforeItems)
+{
+  vector<vector<int>> gGraph(m, vector<int>());
+  vector<vector<int>> iGraph(n, vector<int>());
+
+  // item items do not belong to group to new group
+  for (int i = 0; i < group.size(); i++)
+  {
+    if (group[i] == -1)
+    {
+      // i-th item belongs to no group
+      group[i] = m++;
+    }
+  }
+
+  vector<int> itemInDegree(n, 0);
+  vector<int> groupInDegree(m, 0); // m is increased abrove.
+
+  // build item graph
   for (int i = 0; i < n; i++)
-    cout << nums[i] << endl;
-  // cout << count << endl;
+  {
+    for (int from : beforeItems[i])
+    {
+      itemInDegree[i]++;
+      iGraph[from].push_back(i);
+    }
+  }
+  // build group graph
+  for (int to = 0; to < group.size(); to++)
+  {
+    int toGroup = group[to];
+
+    for (int from : beforeItems[to])
+    {
+      int fromGroup = group[from];
+      if (fromGroup != toGroup)
+      {
+        groupInDegree[toGroup]++;
+        gGraph[fromGroup].push_back(toGroup);
+      }
+    }
+  }
+
+  vector<int> iList = tpSort(iGraph, itemInDegree, n);
+  vector<int> gList = tpSort(gGraph, groupInDegree, m);
+
+  if (iList.size() == 0 || gList.size() == 0)
+    return {};
+
+  vector<vector<int>> groupedList(m, vector<int>());
+
+  for (int item : iList)
+  {
+    int grp = group[item];
+
+    groupedList[grp].push_back(item);
+  }
+
+  vector<int> ans;
+  for (int grp : gList)
+  {
+    for (int item : groupedList[grp])
+    {
+      ans.push_back(item);
+    }
+  }
+  return ans;
+}
+
+int main()
+{
+  int n = 8;
+  int m = 2;
+  vector<int> group = {-1, -1, 1, 0, 0, 1, 0, -1};
+  vector<vector<int>> beforeItems = {{}, {6}, {5}, {6}, {3, 6}, {}, {}, {}};
+
+  auto ans = sortItems(n, m, group, beforeItems);
   return 0;
 }
