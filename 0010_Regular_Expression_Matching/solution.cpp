afa67bc2dcbadd4afd60862a79be06433e2fc630
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-10-28 22:55:20 -0700

    update

--- 0010_Regular_Expression_Matching/solution.cpp
+++ 0010_Regular_Expression_Matching/solution.cpp
@@ -9,6 +9,48 @@ https://leetcode.com/problems/regular-expression-matching/
 using namespace std;
 
 /*
+DP
+f[i][j]表示 s[0..i-1] 是否匹配p[0..j-1]
+
+if p[j - 1] != '*'
+
+     f[i][j] = f[i-1][j-1]&& (p[j-1]=='.'||s[i-1]==p[j-1])
+
+if p[j - 1] == '*', 设 p[j - 2] 为 x
+
+      1) "x*" 重复0次：f[i][j] = f[i][j - 2]
+
+      2)"x*" 重复1次以上：f[i][j] = f[i-1][j] && (p[j-2]=='.'||s[i-1]==p[j-2])
+
+
+*/
+bool isMatch(string s, string p)
+{
+    int m=s.length(), n=p.length();
+    bool f[m+1][n+1];
+    f[0][0] = 1;
+
+    for(int i=1; i<=m; ++i)
+        f[i][0] = 0;
+
+    for(int i=1; i<=n; ++i)
+        if(p[i-1]!='*')
+            f[0][i] = 0;
+        else
+            f[0][i] = f[0][i-2];
+            
+    for(int i=1; i<=m; ++i)
+        for(int j=1; j<=n; ++j)
+        {
+            if(p[j-1]!='*')
+                f[i][j] = f[i-1][j-1]&& (p[j-1]=='.'||s[i-1]==p[j-1]);
+            else
+                f[i][j] = f[i][j-2] || (f[i-1][j]&& (p[j-2]=='.'||s[i-1]==p[j-2]));
+        }
+    return f[m][n];
+}
+
+/*
 https://zxi.mytechroad.com/blog/searching/leetcode-10-regular-expression-matching/
 
 Time complexity: O((|s| + |p|) * 2 ^ (|s| + |p|))
--- 0375_Guess_Number_Higher_or_Lower_II/solution.cpp
+++ 0375_Guess_Number_Higher_or_Lower_II/solution.cpp
@@ -14,6 +14,7 @@ https://leetcode.com/problems/guess-number-higher-or-lower-ii
 #include <unordered_set>
 #include <numeric>
 #include <algorithm>
+#include <functional>
 
 #include "common/ListNode.h"
 #include "common/TreeNode.h"
