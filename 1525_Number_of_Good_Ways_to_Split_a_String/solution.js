7998083923c6ec095f56d766addb95e4280d47a5
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-10-28 09:06:16 -0700

    update

--- 1525_Number_of_Good_Ways_to_Split_a_String/solution.js
+++ 1525_Number_of_Good_Ways_to_Split_a_String/solution.js
@@ -14,9 +14,37 @@ https://leetcode.com/problems/number-of-good-ways-to-split-a-string
  * @param {string} s
  * @return {number}
  */
-var numSplits = function(s) {
+var numSplits = function (s) {
+  let goodSplits = 0;
+  const left = new Array(26).fill(0);
+  const right = new Array(26).fill(0);
+
+  const getDidtinct = (arr) => {
+    return arr.reduce((acc, i) => {
+      if (i > 0) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    right[s.charCodeAt(i) - 97]++;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    left[s.charCodeAt(i) - 97]++;
+    right[s.charCodeAt(i) - 97]--;
+
+    const distinctChartAtLeft = getDidtinct(left);
     
+    const distinctChartAtRight = getDidtinct(right);
+    if (distinctChartAtLeft === distinctChartAtRight) {
+      goodSplits++;
+    }
+  }
+  return goodSplits;
 };
 
-
-export { twoSum };
+// Test
+console.log(numSplits("aacaba"));
